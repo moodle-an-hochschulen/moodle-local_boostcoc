449df5298d85025d527782da23f34f2db70199a1
Alexander Bias <alexander.bias@uni-ulm.de>
2018-03-29 23:37:11 +0200

    Release v3.3-r1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_boostcoc';
-$plugin->version = 2018032900;
-$plugin->release = 'v3.2-r4';
+$plugin->version = 2018032901;
+$plugin->release = 'v3.3-r1';
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('theme_boost' => 2017051500,
